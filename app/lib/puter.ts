0275ce5fe6654fa71b9d4faca20fce8751816576
Prasoon-dwivedi88 <prasoon.dwivedi884@gmail.com>
2026-06-17 14:35:59 +0530

    Update puter.ts changing the ai model to anthropic/claude-haiku-4-5

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -350,7 +350,7 @@ export const usePuterStore = create<PuterStore>((set, get) => {
                     ],
                 },
             ]
-            // ,{ model: "claude-3-haiku" }
+             ,{ model: "anthropic/claude-haiku-4-5" }
         ) as Promise<AIResponse | undefined>;
     };
 
